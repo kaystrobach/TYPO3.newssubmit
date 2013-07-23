dbfc31d840c254c92dbea824fb2e6201ae1e9079
Kay Strobach <github@kay-strobach.de>
2013-07-23 17:01:13 +0200

    [BUG] Fix version compat problem

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,8 +30,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'extbase' => '1.3-4.7',
-			'fluid' => '1.3-4.7',
+			'extbase' => '1.3.0-4.7.99',
+			'fluid' => '1.3.0-4.7.99',
 			'typo3' => '4.5.0-4.7.99',
 		),
 		'conflicts' => array(
